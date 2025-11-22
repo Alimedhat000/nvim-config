b77d8398353dd4a52d4fe4d95b5fc3a625ef1d88
Alimedhat000 <alisensei000@gmail.com>
2025-11-23 01:48:19 +0200

    update

--- lua/plugins/latex.lua
+++ lua/plugins/latex.lua
@@ -3,8 +3,5 @@ return {
   lazy = false,
   init = function()
     vim.g.vimtex_view_method = "general"
-    vim.g.vimtex_view_general_viewer = "open-pdf"
-    vim.g.vimtex_view_general_options = "@pdf"
-    vim.g.vimtex_view_automatic = 1
   end,
 }
